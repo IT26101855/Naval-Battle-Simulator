aa16e5dffd66b159c3040ac4b55f0b0d1751b7cb
Chiran <it26101855@my.sliit.lk>
2026-08-28 15:46:08 +0530

    Adding the title screen to the simulator

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "title.h"
+
+int main(){
+	titlescreen();
+	
+	return 0;
+}	
---
+++ title.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "title.h"
+
+void titlescreen(){
+       system("clear");
+       printf("         ----------------------------------------NAVAL BATTLE SIMULATOR----------------------------------------\n\n\n");
+       printf("Module: Programming Methodology - (SE1012)\n\n");
+       printf("Name: Chiran Herath\n\n");
+       printf("Registration Number: IT26101855\n\n");
+       printf("Faculty of Computing\n\n");
+       printf("Computer Systems Engineering\n\n");
+       printf("Year 1 Semester 1\n\n\n\n\n\n\n\n\n");
+       printf("                                                 Press Enter to Continue... ");
+       getchar();
+       system("clear");
+	
+}
+	
